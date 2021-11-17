cfff1f0f11cdd5c9726ae7cda98fc5fb3e173de3
alu0101209500 <72801013+alu0101209500@users.noreply.github.com>
2021-11-17 12:52:41 +0000

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -78,17 +78,7 @@ async function readMatrixFile(e){
 //Gesión de las operaciones de la matriz de utilidad
 function calculateMatrix(matrix, method, neighbors, prediction) {
     console.log(matrix);
-    switch (Number(method)) {
-        case 2:
-            console.log("Distacia coseno")
-            break;
-        case 3:
-            console.log("Distancia euclidea")
-            break;
-        default:
-            console.log("Método Pearson")
-            break;
-    }
+
     console.log("Vecinos: " + neighbors);
     switch (Number(prediction)) {
         case 2:
@@ -104,6 +94,71 @@ function calculateMatrix(matrix, method, neighbors, prediction) {
 function generateSimilaritiesMatrix(matrix, method) {
     result = [];
     matrix.forEach((_) => {result.push([])});
-    result.forEach((e) => {e.length = matrix[0].length; e.fill(-1)});
+    result.forEach((e) => {e.length = matrix.length; e.fill(-1)});
     console.log(result);
+    for(let i = 0; i < result.length; i++) {
+        result[i][i] = 0;
+    }
+    console.log(result);
+    for(let i = 0; i < result.length; i++) {
+        for(let j = 0; j < result[i].length; j++) {
+            if(j >= i){
+                break;
+            }
+            obtainSimilarityIndex(matrix, i, j, method)
+        }
+    }
 }
+
+function obtainSimilarityIndex(matrix, i, j, method){
+    switch (Number(method)) {
+        case 2:
+            console.log("Distacia coseno")
+            break;
+        case 3:
+            console.log("Distancia euclidea")
+            break;
+        default:
+            console.log("Método Pearson")
+            break;
+    }
+}
+
+function arrayAvg(array){
+    let result = 0;
+    let total = 0;
+    array.forEach((e) => {
+        if(e != "-")
+        result += Number(e);
+        total ++;
+    });
+
+    return (result/total);
+}
+function pearson(matrix, u, v) {
+    let Suv = [];
+    for(let i = 0; i < matrix[0].length; i++) {
+        if(matrix[u][i] !== "-" && matrix[u][i]){
+            Suv.push(i);
+        }
+    }
+    if(Suv.length == 0){
+        return 0;
+    }
+    let t1 = 0, t2 = 0, t3 = 0;
+    for(let i = 0; i < Suv.length; i++) {
+        t1 += (matrix[u][i] - arrayAvg(matrix[u]))*(matrix[v][i] - arrayAvg(matrix[v]));
+    }
+    for(let i = 0; i < Suv.length; i++) {
+        t2 += Math.pow((matrix[u][i] - arrayAvg(matrix[u])), 2);
+    }
+    for(let i = 0; i < Suv.length; i++) {
+        t3 += Math.pow((matrix[v][i] - arrayAvg(matrix[v])), 2);
+    }
+
+    return t1/(t2*t3)
+}
+
+function cosDistance() {}
+
+function euclidean() {}
