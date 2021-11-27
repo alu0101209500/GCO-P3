27a1a831c673277b052b205fcc225798e55933de
alu0101209500 <72801013+alu0101209500@users.noreply.github.com>
2021-11-27 19:31:01 +0000

    Proyecto terminado

--- script/main.js
+++ script/main.js
@@ -74,32 +74,37 @@ async function readMatrixFile(e){
     });
 }
 
-
 //Gesión de las operaciones de la matriz de utilidad
 function calculateMatrix(matrix, method, neighbors, prediction) {
-    console.log(matrix);
+    let modifiedMatrix = [];
+    modifiedMatrix.length = matrix.length;
+    for (let i = 0; i < matrix.length; i++){
+        modifiedMatrix[i] = [...matrix[i]];
+    }
 
-    console.log("Vecinos: " + neighbors);
+    let sim = generateSimilaritiesMatrix(modifiedMatrix, method);
     switch (Number(prediction)) {
         case 2:
-            console.log("Diferencia con la media")
-            break;
+            avgPrediction(modifiedMatrix, sim, neighbors, method)    
+        break;
         default:
-            console.log("Simple")
-            break;
+            simpPrediction(modifiedMatrix, sim, neighbors, method);
+        break;
     }
-    generateSimilaritiesMatrix(matrix, method);
+
+    loadContent(document.getElementById("outputOriginal"), "Matriz original", matrix);
+    loadContent(document.getElementById("outputSimilar"), "Matriz de similitudes", sim);
+    loadContent(document.getElementById("outputComplete"), "Matriz completa", modifiedMatrix);
 }
 
+//Genera una matriz de similitud entre los clientes
 function generateSimilaritiesMatrix(matrix, method) {
     result = [];
     matrix.forEach((_) => {result.push([])});
     result.forEach((e) => {e.length = matrix.length; e.fill(-1)});
-    console.log(result);
     for(let i = 0; i < result.length; i++) {
-        result[i][i] = 0;
+        result[i][i] = -999;
     }
-    console.log(result);
     for(let i = 0; i < result.length; i++) {
         for(let j = 0; j < result[i].length; j++) {
             if(j >= i){
@@ -109,16 +114,17 @@ function generateSimilaritiesMatrix(matrix, method) {
             result[j][i] = result[i][j];
         }
     }
-    console.log(result);
+    return result;
 }
 
+//Permite obtener el índice de similitud entre dos vecinos
 function obtainSimilarityIndex(matrix, i, j, method){
     switch (Number(method)) {
         case 2:
-            console.log("Distacia coseno")
+            return cosDistance(matrix, i, j);
             break;
         case 3:
-            console.log("Distancia euclidea")
+            return euclidean(matrix, i, j);
             break;
         default:
             return pearson(matrix, i, j);
@@ -126,6 +132,7 @@ function obtainSimilarityIndex(matrix, i, j, method){
     }
 }
 
+//Obtiene la media de todos los valores no nulos de una fila
 function arrayAvg(array){
     let result = 0;
     let total = 0;
@@ -141,6 +148,8 @@ function arrayAvg(array){
         return (result/total);
     }
 }
+
+//Obtiene la similitud entre dos usuarios aplicando Pearson
 function pearson(matrix, u, v) {
     let Suv = [];
     for(let i = 0; i < matrix[0].length; i++) {
@@ -154,19 +163,155 @@ function pearson(matrix, u, v) {
     let t1 = 0, t2 = 0, t3 = 0;
     for(let i = 0; i < Suv.length; i++) {
         t1 += (Number(matrix[u][Suv[i]]) - arrayAvg(matrix[u]))*(Number(matrix[v][Suv[i]]) - arrayAvg(matrix[v]));
-    }
-    for(let i = 0; i < Suv.length; i++) {
         t2 += Math.pow((Number(matrix[u][Suv[i]]) - arrayAvg(matrix[u])), 2);
-    }
-    for(let i = 0; i < Suv.length; i++) {
         t3 += Math.pow((Number(matrix[v][Suv[i]]) - arrayAvg(matrix[v])), 2);
     }
     t2 = Math.sqrt(t2);
     t3 = Math.sqrt(t3);
-    console.log("avg = " + arrayAvg(matrix[u]));
     return t1/(t2*t3);
 }
 
-function cosDistance(matrix, u, v) {}
+//Obtiene la similitud entre dos usuarios aplicando distancia coseno
+function cosDistance(matrix, u, v) {
+    let Suv = [];
+    for(let i = 0; i < matrix[0].length; i++) {
+        if(matrix[u][i] !== "-" && matrix[v][i] !== "-"){
+            Suv.push(i);
+        }
+    }
+    if(Suv.length == 0){
+        return 0;
+    }
+    let t1 = 0, t2 = 0, t3 = 0;
+    for(let i = 0; i < Suv.length; i++) {
+        t1 += (Number(matrix[u][Suv[i]]))*(Number(matrix[v][Suv[i]]));
+        t2 += Math.pow((Number(matrix[u][Suv[i]])), 2);
+        t3 += Math.pow((Number(matrix[v][Suv[i]])), 2);
+    }
+    t2 = Math.sqrt(t2);
+    t3 = Math.sqrt(t3);
+    return t1/(t2*t3);
+}
 
-function euclidean() {}
+//Obtiene la similitud entre dos usuarios aplicando distancia euclídea
+function euclidean(matrix, u, v) {
+    let Suv = [];
+    for(let i = 0; i < matrix[0].length; i++) {
+        if(matrix[u][i] !== "-" && matrix[v][i] !== "-"){
+            Suv.push(i);
+        }
+    }
+    if(Suv.length == 0){
+        return 0;
+    }
+    let t1 = 0;
+    for(let i = 0; i < Suv.length; i++) {
+        t1 += Math.pow((Number(matrix[u][Suv[i]])) - (Number(matrix[v][Suv[i]])), 2);
+    }
+    return (-1)*Math.sqrt(t1);
+}
+
+//Rellena los espacios nulos de la matriz aplicando predicción simple
+function simpPrediction(matrix, sim, k, method) {
+    for (let i = 0; i < matrix.length; i++) {
+        if(matrix[i].indexOf("-") !== -1) {
+            let N = [];
+            for(let j = 0; j < k; j++) {
+                let maxval = 0;
+                for (let e = 0; e < matrix[i].length; e++) {
+                    if((Number(sim[i][e]) >= Number(sim[i][maxval])) && (N.indexOf(e) == -1) && (i != e)) {
+                        maxval = e;
+                    }
+                }
+                N.push(maxval);
+            }
+            for (let j = 0; j < matrix[i].length; j++) {
+                if(matrix[i][j] == "-") {
+                    let t1 = 0;
+                    let t2 = 0;        
+                    for(let e = 0; e < N.length; e++) {
+                        if (matrix[N[e]][j] != "-") {
+                            t1 += sim[i][N[e]]*matrix[N[e]][j];
+                            t2 += Math.abs(sim[i][N[e]]);                         
+                        }
+                    }
+                    if (t2 != 0) {
+                        let result = Math.round(t1/t2)
+                        
+                        if (Number(method) == 3) {
+                            result = -1*result; 
+                        }
+                        if (result < 0) {
+                            result = 0;
+                        } else if (result > 5) {
+                            result = 5;
+                        }
+                        
+                        matrix[i][j] = String(result);
+                    }
+                }
+            }
+        }  
+    }
+}
+
+//Rellena los espacios nulos de la matriz aplicando una predicción que evalua la distancia con la media
+function avgPrediction(matrix, sim, k, method) {
+    for (let i = 0; i < matrix.length; i++) {
+        if(matrix[i].indexOf("-") !== -1) {
+            let N = [];
+            for(let j = 0; j < k; j++) {
+                let maxval = 0;
+                for (let e = 0; e < matrix[i].length; e++) {
+                    if((Number(sim[i][e]) >= Number(sim[i][maxval])) && (N.indexOf(e) == -1) && (i != e)) {
+                        maxval = e;
+                    }
+                }
+                N.push(maxval);
+            }
+            for (let j = 0; j < matrix[i].length; j++) {
+                if(matrix[i][j] == "-") {
+                    let t1 = 0;
+                    let t2 = 0;        
+                    for(let e = 0; e < N.length; e++) {
+                        if (matrix[N[e]][j] != "-") {
+                            t1 += sim[i][N[e]]*(matrix[N[e]][j] - arrayAvg(matrix[N[e]]));
+                            t2 += Math.abs(sim[i][N[e]]);
+                        }
+                    }
+                    if (t2 != 0) {
+                        let result = Math.round(t1/t2)
+                        
+                        if (Number(method) == 3) {
+                            result = -1*result; 
+                        }
+                        if (result < 0) {
+                            result = 0;
+                        } else if (result > 5) {
+                            result = 5;
+                        }
+
+                        matrix[i][j] = String(result);
+                    }
+                }
+            }
+        }  
+    }
+}
+
+//Carga el contenido en el DOM de la página
+function loadContent(env, title, matrix){
+    let auxstr = "<h3>" + title + "</h3>";
+    auxstr += "<table>";
+    for (let i = 0; i < matrix.length; i++) {
+        auxstr += "<tr>";
+        for (let j = 0; j < matrix[i].length; j++) {
+            auxstr += "<th>" + String(matrix[i][j]) + "</th>";
+        }
+        auxstr += "</tr>";
+    }
+    auxstr += "</table>";
+
+
+    env.innerHTML = auxstr;
+}
